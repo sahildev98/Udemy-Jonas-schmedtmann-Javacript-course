1b7297d2e3e160e9a12bd1d5085412149ecce3e8
Sahil Ahmed <sahil.98@hotmail.com>
2022-01-11 22:14:06 +0000

    comment added to explain the use of this keyword on (object methods).

--- script.js
+++ script.js
@@ -269,7 +269,9 @@ const sahil = {
     // calcAge: function(age){
     //     return 2021 - age;
     // }
-    /* age is already written as a property of sahil object. This goes against DRY principles(Don't Repeat Yourself). */
+    /* age is already written as a property of sahil object. This goes against DRY principles(Don't Repeat Yourself).
+    To avoid repeating code, we can use the this keyword to execute code that is already written. 
+    in simple words, this refers to the owners obeject or property of an object in this case (below).   */
     calcAge: function(age){
        return 2021 - age;
     }
